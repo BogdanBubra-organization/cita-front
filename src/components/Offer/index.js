c0e7fabb1a60d3466679a227ced3a7cc28f9d054
levan <wrest.levan@gmail.com>
2025-02-10 10:42:38 +0200

    Offer with PFD Viewer

--- src/components/Offer/index.js
+++ src/components/Offer/index.js
@@ -13,9 +13,17 @@ const Offer = () => {
       {/*  className="simple-content"*/}
       {/*/>*/}
 
-      <object data="https://api.citamaster.com/uploads/oferta.pdf" type="application/pdf" width="100%" height="1000px">
-        <p><a style={{textDecoration: "underline"}} href='https://api.citamaster.com/web/service-terms'>{t('url')}</a></p>
-      </object>
+      {/*<object data="https://api.citamaster.com/uploads/oferta.pdf" type="application/pdf" width="100%" height="1000px">*/}
+      {/*  <p><a style={{textDecoration: "underline"}} href='https://api.citamaster.com/web/service-terms'>{t('url')}</a></p>*/}
+      {/*</object>*/}
+
+        <iframe
+            src="https://api.citamaster.com/uploads/oferta.html"
+            width="100%"
+            height="1000px"
+            style={{padding: '16px', border: '1px solid #8e8e8e', borderRadius: '50px'}}
+            frameBorder='0'
+        />
 
     </section>
   )
