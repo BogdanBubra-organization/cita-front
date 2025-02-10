c22db6ece150066e9ec8336a2de08a0b6e707fd0
levan <wrest.levan@gmail.com>
2025-02-10 10:43:31 +0200

    Offer with PFD Viewer

--- src/components/Offer/index.js
+++ src/components/Offer/index.js
@@ -8,15 +8,6 @@ const Offer = () => {
     <section className="container simple-page">
       <h1 className="h2">{t('title')}</h1>
 
-      {/*<div*/}
-      {/*  dangerouslySetInnerHTML={{ __html: t.raw('content') }}*/}
-      {/*  className="simple-content"*/}
-      {/*/>*/}
-
-      {/*<object data="https://api.citamaster.com/uploads/oferta.pdf" type="application/pdf" width="100%" height="1000px">*/}
-      {/*  <p><a style={{textDecoration: "underline"}} href='https://api.citamaster.com/web/service-terms'>{t('url')}</a></p>*/}
-      {/*</object>*/}
-
         <iframe
             src="https://api.citamaster.com/uploads/oferta.html"
             width="100%"
