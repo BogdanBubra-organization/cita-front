2af580e5dad398231cb31ce6cf564ef3fe5adcc1
levan <wrest.levan@gmail.com>
2025-04-03 15:31:55 +0300

    GTM ids added

--- src/components/tracking/GoogleTagManager.js
+++ src/components/tracking/GoogleTagManager.js
@@ -3,7 +3,7 @@
 import { Suspense, useEffect, useState } from 'react'
 import { GoogleTagManagerScripts } from './GoogleTagManagerScripts'
 import { grantConsentForEverything } from './utils.tracking'
-import { GTM_ID } from '@/constants/constants'
+import { GTM_IDS } from '@/constants/constants'
 
 export const GoogleTagManager = ({ status }) => {
   const [isGtagLoaded, setIsGtagLoaded] = useState(false)
@@ -24,22 +24,28 @@ export const GoogleTagManager = ({ status }) => {
   return (
     <>
       <noscript>
-        <iframe
-          src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-          height="0"
-          width="0"
-          style={{
-            display: 'none',
-            visibility: 'hidden',
-          }}
-        ></iframe>
+        {GTM_IDS.map((id) => (
+          <iframe
+            key={id}
+            src={`https://www.googletagmanager.com/ns.html?id=${id}`}
+            height="0"
+            width="0"
+            style={{
+              display: 'none',
+              visibility: 'hidden',
+            }}
+          ></iframe>
+        ))}
       </noscript>
       {/* We need to use Suspense in order to access useSearchParams (see https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout) */}
       <Suspense>
-        <GoogleTagManagerScripts
-          gtmId={GTM_ID}
-          onLoadCallback={() => setIsGtagLoaded(true)}
-        />
+        {GTM_IDS.map((id) => (
+          <GoogleTagManagerScripts
+            key={id}
+            gtmId={id}
+            onLoadCallback={() => setIsGtagLoaded(true)}
+          />
+        ))}
       </Suspense>
     </>
   )
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
 export const BACKEND_API_URL = 'https://api.citamaster.com'
 export const LEGAL_EMAIL = 'legal@citamaster.com'
-export const GTM_ID = 'GTM-W6LS6GBQ'
+export const GTM_IDS = ['GTM-PWPS5ZL5', 'GTM-W6LS6GBQ']
