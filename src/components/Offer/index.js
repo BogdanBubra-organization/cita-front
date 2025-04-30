57ae0d92aa1b2e71266fba88bed4c0d845682a39
levan <wrest.levan@gmail.com>
2025-04-30 09:42:33 +0300

    IBAN changed

--- src/components/Offer/index.js
+++ src/components/Offer/index.js
@@ -9,7 +9,7 @@ const Offer = () => {
       <h1 className="h2">{t('title')}</h1>
 
         <iframe
-            src="https://api.citamaster.com/uploads/oferta.html?v=1.2"
+            src="https://api.citamaster.com/uploads/oferta.html?v=1.4"
             width="100%"
             height="1000px"
             style={{padding: '16px', border: '1px solid #8e8e8e', borderRadius: '50px'}}
