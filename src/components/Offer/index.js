a5eca596fb6a7ede7cb24cde0cdf0b5ef006c2f7
levan <wrest.levan@gmail.com>
2026-04-08 11:02:00 +0300

    Minor fix

--- src/components/Offer/index.js
+++ src/components/Offer/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useTranslations } from 'next-intl'
+import { BACKEND_API_URL } from "@/constants/constants";
 
 const Offer = () => {
   const t = useTranslations('Offer')
@@ -9,7 +10,7 @@ const Offer = () => {
       <h1 className="h2">{t('title')}</h1>
 
         <iframe
-            src="https://api.citamaster.com/web/oferta/8c87d208-5d01-402c-b606-1d7f3425c827?v=1"
+            src={`${BACKEND_API_URL}/web/oferta/8c87d208-5d01-402c-b606-1d7f3425c827?v=1`}
             width="100%"
             height="1000px"
             style={{padding: '16px', border: '1px solid #8e8e8e', borderRadius: '50px'}}
