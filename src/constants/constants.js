84ac933fdc34b3ae05cb4124b491f64b7d70c772
oleksandr-kyrylenko <animetalist@gmail.com>
2026-04-17 15:44:08 +0300

    fix: remove unused gtm

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
 export const BACKEND_API_URL = 'https://api.citamaster.com'
 export const LEGAL_EMAIL = 'legal@citamaster.com'
-export const GTM_IDS = ['GTM-PWPS5ZL5', 'GTM-W6LS6GBQ']
+export const GTM_IDS = ['GTM-W6LS6GBQ']
 export const KOMMO_FORM_ID = process.env.NEXT_PUBLIC_KOMMO_FORM_ID
 export const KOMMO_FORM_HASH = process.env.NEXT_PUBLIC_KOMMO_FORM_HASH
